bd6122f8e15abace9624c6025055bfb79d695c05
QuangDev <ng.quang.coder.new@gmail.com>
2025-06-15 17:55:52 +0700

    create-layout-website

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Ecommerce VN'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'Ecommerce VN is a modern e-commerce platform built with Next.js and Tailwind CSS.'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Shop the best products online'
---
+++ lib/data.ts
@@ -0,0 +1,13 @@
+const data = {
+  headerMenus: [
+    { name: "Today's Deals", href: '/search?tag=today-deals' },
+    { name: 'New Arrivals', href: '/search?tag=new-arrivals' },
+    { name: 'Best Sellers', href: '/search?tag=best-sellers' },
+    { name: 'Browsing History', href: '/#browsing-history' },
+    { name: 'Customer Service', href: '/page/customer-service' },
+    { name: 'About Use', href: '/page/about-us' },
+    { name: 'Help', href: '/page/help' },
+  ],
+}
+
+export default data
