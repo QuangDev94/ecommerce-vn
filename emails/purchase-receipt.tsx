3797b2cf30f8216cc28c0a5921a72d997c175c07
QuangDev <ng.quang.coder.new@gmail.com>
2025-07-01 21:14:36 +0700

    Pay order by Paypal

---
+++ emails/purchase-receipt.tsx
@@ -0,0 +1,148 @@
+import { SERVER_URL } from '@/lib/constants'
+import { IOrder } from '../lib/db/models/order.model.ts'
+import { formatCurrency } from '@/lib/utils'
+import {
+  Body,
+  Column,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Img,
+  Preview,
+  Row,
+  Section,
+  Tailwind,
+  Text,
+} from '@react-email/components'
+
+type OrderInformationProps = {
+  order: IOrder
+}
+PurchaseReceipEmail.PreviewProps = {
+  order: {
+    _id: '1234',
+    isPaid: true,
+    paidAt: new Date(),
+    totalPrice: 100,
+    itemsPrice: 100,
+    taxPrice: 0,
+    shippingPrice: 0,
+    user: {
+      name: 'Quang Dev',
+      email: 'qagnguyen.dev@gmail.com',
+    },
+    shippingAddress: {
+      fullName: 'QuangDev',
+      street: '123 Main St',
+      city: 'New York',
+      postalCode: '12345',
+      country: 'USA',
+      phone: '123-456-7890',
+      province: 'New York',
+    },
+    items: [
+      {
+        clientId: '123',
+        name: 'Product 1',
+        image: 'https://via.placeholder.com/150',
+        price: 100,
+        quantity: 1,
+        product: '123',
+        slug: 'product-1',
+        category: 'Category 1',
+        countInStock: 10,
+      },
+    ],
+    paymentMethod: 'Paypal',
+    expectedDeliveryDate: new Date(),
+    isDelivered: true,
+  } as IOrder,
+} satisfies OrderInformationProps
+
+const dateFormatter = new Intl.DateTimeFormat('en', { dateStyle: 'medium' })
+console.log(
+  PurchaseReceipEmail.PreviewProps.order.items[0].image.startsWith('/')
+    ? `${SERVER_URL}${PurchaseReceipEmail.PreviewProps.order.items[0].image}`
+    : PurchaseReceipEmail.PreviewProps.order.items[0].image,
+)
+export default function PurchaseReceipEmail({ order }: OrderInformationProps) {
+  return (
+    <Html>
+      <Preview>View order receipt</Preview>
+      <Tailwind>
+        <Head />
+        <Body className='font-sans bg-white'>
+          {/* max-width: 36rem; 576px */}
+          <Container className='max-w-xl'>
+            <Heading>Purchase Receipt</Heading>
+            <Section>
+              <Row>
+                <Column>
+                  <Text className='mb-0 mr-4 text-gray-500 whitespace-nowrap text-nowrap'>
+                    OrderId
+                  </Text>
+                  <Text className='mt-0 mr-4'>{order._id.toString()}</Text>
+                </Column>
+                <Column>
+                  <Text className='mb-0 mr-4 text-gray-500 whitespace-nowrap text-nowrap'>
+                    Purchased On
+                  </Text>
+                  <Text className='mt-0 mr-4'>
+                    {dateFormatter.format(order.createdAt)}
+                  </Text>
+                </Column>
+                <Column>
+                  <Text className='mb-0 mr-4 text-gray-500 whitespace-nowrap text-nowrap'>
+                    Price Paid
+                  </Text>
+                  <Text className='mt-0 mr-4'>
+                    {formatCurrency(order.totalPrice)}
+                  </Text>
+                </Column>
+              </Row>
+            </Section>
+            <Section className='border border-solid border-gray-500 rounded-lg p-4 md:p-6 my-4'>
+              {order.items.map((item) => (
+                <Row key={item.product} className='mt-4'>
+                  <Column className='w-20'>
+                    <Img
+                      width='80'
+                      alt={item.name}
+                      src={
+                        item.image.startsWith('/')
+                          ? `${SERVER_URL}${item.image}`
+                          : item.image
+                      }
+                    />
+                  </Column>
+                  <Column className='align-top'>
+                    <Text className='mx-2 my-0'>
+                      {item.name} x {item.quantity}
+                    </Text>
+                  </Column>
+                  <Column align='right' className='align-top'>
+                    <Text className='m-0'>{formatCurrency(item.price)}</Text>
+                  </Column>
+                </Row>
+              ))}
+              {[
+                { name: 'Items', price: order.itemsPrice },
+                { name: 'Tax', price: order.taxPrice },
+                { name: 'Shipping', price: order.shippingPrice },
+                { name: 'TotalPrice', price: order.totalPrice },
+              ].map((item) => (
+                <Row key={item.name} className='py-1'>
+                  <Column align='right'>{item.name} :</Column>
+                  <Column align='right' width={90} className='align-top'>
+                    <Text className='m-0'>{formatCurrency(item.price)}</Text>
+                  </Column>
+                </Row>
+              ))}
+            </Section>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  )
+}
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,8 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Ecommerce VN'
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+export const SENDER_EMAIL = process.env.SENDER_EMAIL || 'onboarding@resend.dev'
+export const SENDER_NAME = process.env.SENDER_NAME || APP_NAME
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   'Ecommerce VN is a modern e-commerce platform built with Next.js and Tailwind CSS.'
